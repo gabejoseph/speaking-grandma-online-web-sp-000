cc05dcf0701902f43ac622e29c0e06c2ebf40f33
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-03-27 14:58:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(speak)
-  if speak == 
+  if speak != speak.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif
+  elsif speak == speak.upcase
     return "NO, NOT SINCE 1938!"
   else 
+    speak == "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN"
   end
 end
